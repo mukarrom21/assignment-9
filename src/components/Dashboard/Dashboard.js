b94bf8a670c47b650be3be662e7d0cf4c5f987eb
mukarrom21 <mukarrom21@gmail.com>
2022-04-04 20:00:45 +0600

    rechart installed

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import MyLineChart from '../MyLineChart/MyLineChart';
+import MyBarChart from '../MyBarChart/MyBarChart';
 const Dashboard = () => {
     return (
-        <div className='border-2 rounded-[15px] bg-red-200 text-red-900 w-3/4 mx-auto mt-10 p-10 text-3xl font-bold'>
-            <h1>This is dashboard page</h1>
+        <div className=''>
+            <MyLineChart></MyLineChart>
+            <MyBarChart></MyBarChart>
         </div>
     );
 };
